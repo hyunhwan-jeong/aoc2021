d2b3b6b6ce656ae988b4aaf4067596f9692f98d7
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-01 21:06:24 -0600

    day01-02 solved.

--- day01.groovy
+++ day01.groovy
@@ -4,10 +4,22 @@ new File("day01.in").eachLine { it ->
   inputs.add(it as int)
 }
 
-def ret = 0
+def ret1 = 0
 
 for(i in 0..inputs.size()-1) {
-    if(inputs[i] < inputs[i+1]) ret += 1
+    if(inputs[i] < inputs[i+1]) ret1 += 1
+}
+println ret1
+
+def windows = []
+
+for(i in 0..inputs.size()-3) {
+    windows.add(inputs[i..i+2].sum())
 }
 
-println ret
+def ret2 = 0
+for(i in 0..windows.size()-1) {
+    if(windows[i] < windows[i+1]) ret2 += 1
+}
+
+println ret2
