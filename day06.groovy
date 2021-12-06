cf91c0b51fb17412179fb622733db3f812695d4a
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-06 00:43:28 -0600

    solved the second task of day 6.

--- day06.groovy
+++ day06.groovy
@@ -21,3 +21,27 @@ for(i in 1..80) {
 
 println vec.size()
 
+
+// solution for part 2
+vec = new File("day06.in").readLines().get(0).split(",").collect(it->it as int)
+
+freq = new long[9]
+
+for(v in vec) freq[v]++
+
+for(i in 1..256) {
+    next = new long[9]
+
+    for(j in 0..7) {
+        next[j] = freq[j+1]
+    }
+    next[8] = freq[0]
+    next[6] += freq[0]
+
+    for(j in 0..8) freq[j] = next[j]
+}
+
+// println freq.sum()
+// println 26984457539 == freq.sum()
+
+println freq.sum()
