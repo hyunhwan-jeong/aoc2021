f62de4570a22b8608961994e197ebc35f2bfa3d6
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-09 15:39:06 -0600

    solved the second problem of day 09. used BFS.

--- day09.groovy
+++ day09.groovy
@@ -10,7 +10,8 @@ M = B[0].size()
 di = [0, 0, -1, 1]
 dj = [-1, 1, 0, 0]
 
-ret = 0
+ret_task1 = 0
+ret_task2 = []
 for(i in 0..N-1) {
     for(j in 0..M-1) {
         islow = true
@@ -27,9 +28,40 @@ for(i in 0..N-1) {
             }
         }
         if(islow) {
-            ret += B[i][j] + 1
+            ret_task1 += B[i][j] + 1
+            ret_task2.add(bfs(i, j))
         }
     }
 }
 
-println ret
+ret_task2.sort()
+
+def bfs(si, sj) {
+    ret = 0
+    visited = new boolean[N][M]
+
+    q = [] as Queue
+    q << [si, sj]
+    while(!q.isEmpty()) {
+        (i, j) = q.poll()
+        if(visited[i][j]) continue
+        visited[i][j] = true
+        ret++
+        for(k in 0..3) {
+            ni = i + di[k]
+            nj = j + dj[k]
+
+            if(ni < 0 || ni >= N) continue
+            if(nj < 0 || nj >= M) continue
+            if(B[ni][nj] == 9) continue
+
+            // if(B[ni][nj] == 1 + B[i][j]) {
+            if(B[ni][nj] > B[i][j]) {
+                q << [ni, nj]
+            }
+        }
+    }
+    return ret
+}
+println ret_task1
+println ret_task2[-3] * ret_task2[-2] * ret_task2[-1]
