374caf26af67aca91438eb472a9702ec00a34f25
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-10 05:24:55 -0600

    solved 2nd of day10, integer overflow was an issue.

--- day10.groovy
+++ day10.groovy
@@ -1,26 +1,45 @@
 match = [ ")": "(", "]": "[", ">": "<", "}" : "{" ]
 open = [ "(", "[", "<", "{" ] as Set
 score = [ ")" : 3, "]" : 57, "}": 1197, ">": 25137 ] 
-
-ret = 0
+score2 = [ "<" : 4, "{": 3, "[": 2, "(": 1]
+ret_task1 = 0
+ret_task2 = []
 new File("day10.in").eachLine { line ->
     line = [line.toCharArray()].flatten()
     stk = [] as Stack
+
+    corrupted = false
     for(c in line) {
         c = c as String
         if(open.contains(c)) {
             stk.push(c as String)
         } else {
             if(stk.isEmpty()) {
+                corrupted = true
                 break
             }
             top = stk.pop()
             if(match[c] != top) {
-                ret += score[c]
+                ret_task1 += score[c]
+                corrupted = true
                 break         
             }
         }
     }
-}
 
-println ret
+    if(!corrupted && !stk.isEmpty()) {
+        cur_score2 = 0 as BigInteger
+        while(!stk.empty()) {
+            cur_score2 *= 5 as BigInteger
+            cur_score2 += score2[stk.pop()] as BigInteger
+        }
+        ret_task2.add(cur_score2)
+    }
+    
+}
+ret_task2.sort()
+
+println ret_task1
+println ret_task2.size()
+println ret_task2
+println ret_task2[ret_task2.size().intdiv(2)]
