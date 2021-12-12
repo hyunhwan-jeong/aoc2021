18f2fd2283f3accf61187a31ea98976bc85e76c5
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-12 16:13:52 -0600

    solved day12's 2nd problem.

--- day12.groovy
+++ day12.groovy
@@ -42,4 +42,49 @@ def traverse(where, path) {
     }
 }
 
+
+n_visited = [:]
+graph.each {
+    it -> n_visited[it.key] = 0
+}
+
+println "traverse2"
+ret = 0
+traverse2("start", ["start"], false)
+
+def traverse2(where, path, is_twiced) {
+    if(where=="end") {
+        ret++
+        println path
+    } else if(is_lower[where]) {
+        if(where == "start" || where == "end") {
+            if(n_visited[where] == 0) {
+                n_visited[where] = 1
+                graph[where].each {
+                    it -> traverse2(it, path + [it], is_twiced)
+                }
+                n_visited[where] = 0
+            }
+        }
+        else if(!is_twiced && n_visited[where] == 1) {
+            n_visited[where] = 2
+            graph[where].each {
+                it -> traverse2(it, path + [it], true)
+            }
+            is_twiced= false
+            n_visited[where] = 1
+        } else if(n_visited[where] == 0) {
+            n_visited[where] = 1
+            graph[where].each {
+                it -> traverse2(it, path + [it], is_twiced)
+            }
+            n_visited[where] = 0
+        } 
+    } else {
+        graph[where].each {
+            it -> traverse2(it, path + [it], is_twiced)
+        }
+    }
+}
+
 println ret
