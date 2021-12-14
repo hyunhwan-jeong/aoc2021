0b37777bfa0ef7e5f86320e58f083ef7582c2551
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-14 00:06:22 -0600

    solved the second task of day 14.

--- day14.groovy
+++ day14.groovy
@@ -3,7 +3,7 @@ is_rule = false
 polymer = null
 rule = [:]
 
-new File("day14.in").eachLine {
+new File("day14_sample.in").eachLine {
     if(it == "") {
         is_rule = true
     } else if(is_rule) {
@@ -15,6 +15,7 @@ new File("day14.in").eachLine {
 }
 
 for(step in 1..10) {
+    println "steps... $step"
     new_polymer = ""
     for(i in 0..polymer.length()-2) {
         cur = polymer.substring(i, i+2) as String
@@ -25,7 +26,7 @@ for(step in 1..10) {
         new_polymer += polymer.substring(i+1, i+2)
     }
     polymer = new_polymer
-    println polymer
+    // println polymer
 }
 
 uniq = polymer.toCharArray() as Set
@@ -40,3 +41,73 @@ uniq.each { it1 ->
 
 ret.sort()
 println ret[ret.size()-1] - ret[0]
+
+is_rule = false
+new File("day14.in").eachLine {
+    if(it == "") {
+        is_rule = true
+    } else if(is_rule) {
+        (key, value) = it.split( " -> " )
+        rule[key as String] = value
+    } else {
+        polymer = it 
+    }
+}
+
+rule[" " + polymer.substring(0, 1)] = polymer.substring(0, 1)
+pair_cnt = [:]
+
+polymer = " " + polymer
+
+for(i in 0..polymer.length()-2) {
+    cur = polymer.substring(i, i+2) as String
+    if(pair_cnt[cur] == null) {
+        pair_cnt[cur] = 1 as long
+    } else {
+        pair_cnt[cur] = pair_cnt[cur] + 1 as long
+    }
+}
+
+for(step in 1..40) {
+    next_pair_cnt = [:]
+    for(e in pair_cnt) {
+        k = e.key
+        v = e.value as long
+        if(k.substring(0,1) != " ") {
+            new_key = k.substring(0, 1) + rule[k]
+
+            if(next_pair_cnt[new_key] == null) {
+                next_pair_cnt[new_key] = v 
+            } else {
+                next_pair_cnt[new_key] += v 
+            }
+            new_key = rule[k] + k.substring(1, 2)
+
+            if(next_pair_cnt[new_key] == null) {
+                next_pair_cnt[new_key] = v
+            } else {
+                next_pair_cnt[new_key] += v 
+            }
+        } else {
+            next_pair_cnt[k] = 1 as long
+        }
+    }
+
+    pair_cnt = next_pair_cnt.clone()
+
+    count = [:]
+    for(e in pair_cnt) {
+        c = e.key.toCharArray()[1] as String
+        v = e.value as long
+        println "$e.key $c $v"
+        if(count[c] == null) {
+            count[c] = v 
+        } else {
+            count[c] += v 
+        }
+    }
+    println count
+    println count.collect{ it.value }.max() - count.collect{ it.value }.min()
+}
+
+
