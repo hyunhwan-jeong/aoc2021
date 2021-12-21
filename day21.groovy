793bd030dac87399c22fe8c62acf59e6770155bc
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-20 23:14:37 -0600

    optimized, but it doesn't help Part 2.

--- day21.groovy
+++ day21.groovy
@@ -3,8 +3,8 @@ new File("day21.in").withReader { r ->
     p[0] = r.readLine().split(": ")[1] as int
     p[1] = r.readLine().split(": ")[1] as int    
 }
-//p[0]--
-//p[1]--
+p[0]--
+p[1]--
 
 die = 1
 roll = 0
@@ -12,17 +12,12 @@ turn = 0
 
 s = [0,0]
 while(true) {
-    // p[turn] = (p[turn] + 3 * die + 3) % 10
+    p[turn] = (p[turn] + 3 * die + 3) % 10
 
     dice = 3 * die + 3
-    for(i in 1..dice) {
-        p[turn]++
-        if(p[turn] > 10) p[turn] = 1
-    }
     die += 3
     roll += 3
-    //s[turn] += p[turn]+1
-    s[turn] += p[turn]
+    s[turn] += p[turn]+1
 
     if(s[turn]>=1000) break
     turn = 1-turn
