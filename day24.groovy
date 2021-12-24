4ac3a0d175260d7f36251a86bfb718a913f0a805
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-24 02:00:35 -0600

    The day 24 problem was mainly solved by by hand, but I have a groovy solution.

--- day24.groovy
+++ day24.groovy
@@ -1,43 +1,69 @@
 cmd = []
 new File("day24.in").eachLine { line ->
-    cmd.add(line.split(" "))
-}
-
-def solve(test) {
-    value = ["x": 0G, "y": 0G, "z": 0G, "w": 0G]
-    def j = 0
-    for(c in cmd) {
-        if(c[0] == "inp") {
-            //println "========================================"
-            value["w"] = test[j++] as BigInteger
-            //println "$c: $value"
-            continue
-        } 
-
-        b = 0G
-        if(c[2] != "x" && c[2] != "y" && c[2] != "z" && c[2] != "w") {
-            b = c[2] as BigInteger
-        } else {
-            b = value[c[2]]
-        }
-
-        if(c[0] == "add") value[c[1]] = value[c[1]].add(b)
-        else if(c[0] == "mul") value[c[1]] = value[c[1]].multiply(b)
-        else if(c[0] == "div") value[c[1]] = value[c[1]].divide(b)
-        else if(c[0] == "mod") value[c[1]] = value[c[1]].mod(b)
-        else if(c[0] == "equ") {
-            if(value[c[1]] == b) value[c[1]] = 1G
-            else value[c[1]] = 0G
-        }
-        // println "$c: $value"
-        //println "$c: $value"
+    if(line == "inp w") {
+        cmd.add([])
+    } else {
+        cmd[cmd.size()-1].add(line.split(" "))
     }
-    println "$value"
 }
 
-solve([1,1,1,1,1,1,1,1,1,1,1,1,1,1])
-solve([2,2,2,2,2,2,2,2,2,2,2,2,2,2])
-solve([3,3,3,3,3,3,3,3,3,3,3,3,3,3])
-solve([4,4,4,4,4,4,4,4,4,4,4,4,4,4])
-solve([9,9,9,9,9,9,9,9,9,9,9,9,9,9])
-solve([1,3,5,7,9,2,4,6,8,9,9,9,9,9])
+def z = 0
+
+weight = []
+
+for(c in cmd) {
+    def alpha = c[4][2] as long
+    def beta = c[3][2] as long
+    def gamma = c[14][2] as long
+    weight.add([alpha, beta, gamma])
+    //println("-----")
+}
+
+def calc_z(w, z, weight) {
+    (alpha, beta, gamma) = weight
+    def x = (z%26+alpha) != w ? 1: 0
+    def y = 25 * x + 1
+    z = z.intdiv(beta) * y + (w+gamma) * x
+}
+
+W = new int[14]
+def solve_front(i, z, mode = "min") {
+    if(i>=8) {
+        //println "${W[0..7]} $z"
+        if(z==0) println W[0..7].collect(it->it as String).join("")
+        return z==0
+    }
+    else {
+        digits = [1,2,3,4,5,6,7,8,9]
+        if( mode == "min" ) digits.reverse()
+        for(w in digits) {
+            W[i] = w
+            if(solve_front(i+1, calc_z(W[i],z, weight[i]), mode)) return true
+        }
+    }
+    return false 
+} 
+
+def solve_back(i, z) {
+    if(i>=14) {
+        //println "${W[0..7]} $z"
+        if(z==0) println W[8..13].collect(it->it as String).join("")
+        return z==0
+    }
+    else {
+        for(w in 1..9) {
+            W[i] = w
+            if(solve_back(i+1, calc_z(W[i],z, weight[i]))) return true
+        }
+    }
+    return false 
+} 
+
+print "first 8 digits of maximum: "
+solve_front(0, 0, "max")
+print "first 8 digits of maximum: "
+solve_front(0, 0, "min")
+print "the final digits: " 
+solve_back(8, 0)
+
+
