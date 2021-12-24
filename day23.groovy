cff2934ebac40a701bd2e114f51a8b057618bfab
Hyun-Hwan Jeong <jeong.compbio@gmail.com>
2021-12-23 22:46:28 -0600

    I made it faster.

--- day23.groovy
+++ day23.groovy
@@ -19,7 +19,7 @@ for(i in 0..N-1) for(j in 0..M-1) {
     if(board[i][j] != '#') {
         index[i][j] = K++
         if(board[i][j] != '.') {
-            println board[i][j] as char
+            // println board[i][j] as char
             start_state[(board[i][j] as char)-('A' as char)].add(index[i][j])
         }
     }
@@ -34,10 +34,10 @@ for(i in 0..N-1) for(j in 0..M-1) if(index[i][j] != -1) {
     }
 }
 
-println dist
+// println dist
 
 
-print start_state
+// print start_state
 near = [] 
 
 
@@ -48,20 +48,10 @@ for(i in 0..K-1) {
 di = [-1,1,0,0]
 dj = [0,0,-1,1]
 
-for(i in 0..N-1) for(j in 0..M-1) if(index[i][j]!=-1) {
-    for(k in 0..3) {
-        ii = i + di[k]
-        jj = j + dj[k]
-        if(ii>=0 && ii<N && jj>=0 && jj<M && index[ii][jj]!=-1) {
-            near[index[i][j]].add(index[ii][jj])
-        }
-    }
-}
-
-println near
+// println near
 
 GOAL = [[11,15], [12,16], [13,17], [14,18]]
-println GOAL
+// println GOAL
 cache = [:]
 
 import groovy.transform.Canonical
@@ -71,8 +61,9 @@ cost = [1, 10, 100, 1000]
 def calc_approx(cur_state) {
     def cur_dist = 0
     for(i in 0..3) {
-        cur_dist += Math.min( dist[cur_state[i][0]][GOAL[i][0]] + dist[cur_state[i][1]][GOAL[i][1]],
-                              dist[cur_state[i][0]][GOAL[i][1]] + dist[cur_state[i][1]][GOAL[i][0]]) * cost[i]
+        for(j in 0..1) {
+            cur_dist += dist[cur_state[i][j]][GOAL[i][1]] * 2 * cost[i]
+        }
     }
     cur_dist
 }
@@ -88,9 +79,9 @@ class Node implements Comparable<Node> {
 }
 
 println "search starts"
-println calc_approx(start_state)
+// println calc_approx(start_state)
 pq = new PriorityQueue<Node>()
-pq.add(new Node(cost: 0, state: start_state, approx : calc_approx(start_state)))
+pq.add(new Node(cost: 0, state: start_state, approx : 0))
 cache[start_state] = calc_approx(start_state)
 
 
@@ -98,6 +89,64 @@ best_approx = 987654321
 best_node = null
 
 timer = 0 
+
+def add_node(pq, new_state, node, kind, move_cost) {
+    //println "${node.state[kind]} ${new_state[kind]} $move_cost"
+    def cur_approx = calc_approx(new_state)
+    def new_cost = node.cost + move_cost * cost[kind]
+    if(!cache.containsKey(new_state) || cache[new_state] > new_cost) {
+        cache[new_state] = new_cost 
+        pq.add(new Node(cost: new_cost, state: new_state, approx: 0))
+    }
+}
+
+def is_feasible(where, target, v) {
+    //println "$where, $target, $v" 
+    def visited = new int[K]
+    for(j in 0..K-1) visited[j] = v[j]
+    visited[where] = -1
+    visited[target] = -1
+    /*
+    println "$where -> $target"
+    print "#"
+    for(j in 0..10) if(visited[j]==-1) print "." else print visited[j] 
+    println "#"
+    print "###"
+    for(j in 11..14) {
+        if(visited[j]==-1) print "." else print "${visited[j]}"
+        if(j != 14) print "#"
+    }
+    println "###"
+    print "###"
+    for(j in 15..18) {
+        if(visited[j]==-1) print "." else print "${visited[j]}"
+        if(j != 18) print "#"
+    }
+    println "###"
+    */
+    
+    while(true) {
+        // println "current: $where"
+        if(where >= 15) {
+            where -= 4
+        } else if(where >= 11) {
+            where = (where - 10) * 2
+        } else {
+            if(target < where) where--
+            else where++
+        }
+        //println "$where: ${visited[where]}"
+        if(visited[where] != -1) {
+            return false
+        }
+        if(where == target) {
+            break
+        }
+    }
+    //print("feasible\n")
+    return true
+}
+
 while(!pq.isEmpty()) {
     def node = pq.poll()
     def state = node.state
@@ -107,75 +156,73 @@ while(!pq.isEmpty()) {
     }
     if(cache[state] < node.cost) continue
 
-    if(node.approx < best_approx) {
-        best_approx = node.approx
+    /*if(node.cost < best_approx) {
+        best_approx = node.cost
         best_node = node
-    }
-    if(timer++ % 1000000 == 0) {
-        println "at $timer = $best_node.state: $best_node.cost [estimated distance: ${best_node.approx}]"
+    }*/
+    if(timer++ % 10000 == 0) {
+        // println "at $timer = $best_node.state: $best_node.cost [estimated distance: ${best_node.approx}]"
+        println "at $timer, searching a node with $node.cost"
     }
 
-    def visited = new boolean[K]
+    def visited = new int[K]
     // println "testing... at $node.cost"
 
-    for(s in state) {
-        visited[s[0]] = true
-        visited[s[1]] = true
+    Arrays.fill(visited, -1)
+    for(i in 0..3) {
+        for(j in 0..1) {
+            visited[state[i][j]] = i
+        }
     }
-
-    def n_at_top = 0
-    for(i in 0..10) if(visited[i]) ++n_at_top
 
 
     for(i in 0..3) {
         if(state[i]==GOAL[i]) continue
-        def a = state[i][0]
-        def b = state[i][1]
-        if(a != GOAL[i][1]) {
-            for(next in near[a]) {
-                if(visited[next]) continue
-                if(a > 10 && next >= 0 && next <= 10 && n_at_top >= 4) continue
+        for(x in 0..1) {
+            if(state[i][x] <= 10) {
+                for(j in 1..0) {
+                    if(visited[GOAL[i][j]] == i) continue
+                    if(visited[GOAL[i][j]] == -1) {
+                        if(is_feasible(GOAL[i][j], state[i][x], visited)) {
+                            def new_state = [state[0].clone(), state[1].clone(), state[2].clone(), state[3].clone()]
+                            new_state[i][x] = GOAL[i][j]
+                            new_state[i].sort()
+                            //println "case 1: ${state[i]} ${new_state[i]}"
 
-                def new_state = [state[0], state[1], state[2], state[3]] 
-                // why is this a problem? new_state[i] = next < b ? (next << 32L) + b : (b << 32L) + next
-                if (next < b) {
-                    new_state[i] = [next, b]
-                } else {
-                    new_state[i] = [b, next]
+                            
+                            add_node(pq, new_state, node, i, dist[state[i][x]][GOAL[i][j]])
+                        }
+                    }
+                    break
                 }
-                
-                def cur_approx = calc_approx(new_state)
-                if(node.cost + cost[i] + cur_approx >= 13499) continue
-
-                if(cache.containsKey(new_state) && cache[new_state] <= node.cost + cost[i]) continue
-                cache[new_state] = node.cost + cost[i]
-
-                //println "($a and $b) => $next and $b, cost: ${cache[new_state]}, new_state = ${new_state[i]}"
-
-                pq.add(new Node(cost: cache[new_state], state: new_state, approx: cur_approx))
-            }
-        }
-        if(b != GOAL[i][1]) {
-            for(next in near[b]) {
-                if(visited[next]) continue
-                if(b > 10 && next >= 0 && next <= 10 && n_at_top >= 4) continue
-
-                def new_state = [state[0], state[1], state[2], state[3]] 
-
-                if (next < a) {
-                    new_state[i] = [next, a]
-                } else {
-                    new_state[i] = [a, next]
+            } else {
+                def is_stable = false
+                def is_obstacle = false
+                for(j in 1..0) {
+                    if(state[i][x] == GOAL[i][j]) {
+                        is_stable = true
+                        break
+                    }
+                    if(visited[GOAL[i][j]] != i) {
+                        break
+                    }                        
                 }
 
-                def cur_approx = calc_approx(new_state)
-                if(node.cost + cost[i] + cur_approx >= 13499) continue
-                if(cache.containsKey(new_state) && cache[new_state] <= node.cost + cost[i]) continue
+                if(is_stable) continue
 
-                cache[new_state] = node.cost + cost[i] 
+                for(target in 0..10) if(visited[target] == -1) {
+                    def where = state[i][x]
+                   
+                    if(!is_feasible(state[i][x], target, visited)) continue
+                    //println "case 2: ${state[i]}"
+                    def new_state = [state[0].clone(), state[1].clone(), state[2].clone(), state[3].clone()]
+                    new_state[i][x] = target
+                    new_state[i].sort()
+                    //println "case 2: ${state[i]} ${new_state[i]}"
 
-                //println "($a and $b) => $next and $a, cost: ${cache[new_state]}, new_state = ${new_state[i]}"
-                pq.add(new Node(cost: cache[new_state], state: new_state, approx: cur_approx))
+                    add_node(pq, new_state, node, i, dist[state[i][x]][target])
+
+                }
             }
         }
     }
